83b6e084803574de5e4604bfc82e0a9b170aabaa
vikas0713 <vikas0713@gmail.com>
2026-09-24 09:10:29 +0530

    Add Divine Aperture brand logo

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -1,9 +1,9 @@
-import { Aperture } from 'lucide-react'
+import logo from '../assets/divine-aperture-logo.png'
 
 export function Brand({ compact = false }: { compact?: boolean }) {
   return (
     <div className="brand">
-      <div className="brand-mark"><Aperture size={compact ? 18 : 22} strokeWidth={1.2} /></div>
+      <div className="brand-mark"><img className="brand-logo" src={logo} alt="Divine Aperture Studio" /></div>
       {!compact && <div><span>Divine Aperture</span><small>Studio</small></div>}
     </div>
   )
